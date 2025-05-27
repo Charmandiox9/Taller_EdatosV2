efa6d88a12bc737bb8783639fa6af02cfff6a08f
Marton1123 <131730737+Marton1123@users.noreply.github.com>
2025-05-26 23:39:04 -0400

    test

--- Dominio/Nodos/NodoSistema.cpp
+++ Dominio/Nodos/NodoSistema.cpp
@@ -39,7 +39,7 @@ NodoSistema* NodoSistema::getAnterior() const {
 }
 
 void NodoSistema::setTanque(Tanque* nuevoTanque) {
-    // NO liberamos memoria aquí. Asumimos que el control de vida del tanque está fuera del nodo.
+    
     tanque = nuevoTanque;
 }
 
