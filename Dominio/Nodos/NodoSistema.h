005ce0739552356d129b26eaf9c62c6583350801
Daniel Durán <107888507+Charmandiox9@users.noreply.github.com>
2025-05-26 23:35:55 -0400

    Revert "mejoras menuAccionesJugador"

--- Dominio/Nodos/NodoSistema.h
+++ Dominio/Nodos/NodoSistema.h
@@ -3,39 +3,34 @@
 
 #include "../Tanques/Tanque.h"
 
-class NodoSistema {
-private:
-    int idNodo;
-    int posicionX;
-    int posicionY;
-    int tipoTerreno; // 1: planicie, 2: bosque, 3: montaña, 0: agua
+class NodoSistema{
+    private:
+        int idNodo;
+        int posicionX;
+        int posicionY;
+        Tanque* tanque;
+        int tipoTerreno; // 1: planicie, 2: bosque, 3: montaña, 0: agua
+        NodoSistema* siguiente; // Puntero a siguiente nodo (opcional, si se necesita una lista enlazada)
+        NodoSistema* anterior; // Puntero a nodo anterior (opcional, si se necesita una lista enlazada)
+    public:
+        NodoSistema(int id, int x, int y, int tipoTerreno);
 
-    Tanque* tanque; // Puntero al tanque ubicado en este nodo (puede ser nullptr)
-    NodoSistema* siguiente;
-    NodoSistema* anterior;
+        int getIdNodo() const;
+        int getPosX() const;
+        int getPosY() const;
+        int getTipoTerreno() const;
+        Tanque* getTanque() const;
+        NodoSistema* getSiguiente() const; // Método para obtener el siguiente nodo
+        NodoSistema* getAnterior() const; // Método para obtener el nodo anterior
+        void destruirTanque();
 
-public:
-    NodoSistema(int id, int x, int y, int tipoTerreno);
-
-    // Getters
-    int getIdNodo() const;
-    int getPosX() const;
-    int getPosY() const;
-    int getTipoTerreno() const;
-    Tanque* getTanque() const;
-    NodoSistema* getSiguiente() const;
-    NodoSistema* getAnterior() const;
-
-    // Setters
-    void setTanque(Tanque* nuevoTanque);
-    void setPosX(int nuevoX);
-    void setPosY(int nuevoY);
-    void setTipoTerreno(int nuevoTipoTerreno);
-    void setSiguiente(NodoSistema* siguienteNodo);
-    void setAnterior(NodoSistema* anteriorNodo);
-
-    // Gestión de memoria
-    void destruirTanque(); // Elimina el tanque asociado solo si fue creado dentro del nodo
+        void setTanque(Tanque* nuevoTanque);
+        void setPosX(int nuevoX);
+        void setPosY(int nuevoY);
+        void setTipoTerreno(int nuevoTipoTerreno);
+        void setSiguiente(NodoSistema* siguienteNodo); // Método para establecer el siguiente nodo
+        void setAnterior(NodoSistema* anteriorNodo); // Método para establecer el nodo anterior
 };
+
 
 #endif
